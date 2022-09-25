8b0056104b62338bcb6dcd5c07aa500cb046eb7a
Bishal Khanal <bishal.khanal87@gmail.com>
2022-09-24 23:18:45 -0700

    adds add task feature

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () =>{
   const [tasks, setTasks] = useState([
     
-      {
-        id: 1,
-        text: 'Doctors Appt',
-        day: '1st of feb',
-        remider: true,
-      },
+      // {
+      //   id: 1,
+      //   text: 'Doctors Appt',
+      //   day: '1st of feb',
+      //   reminder: true,
+      // },
       
-      {
-        id: 2,
-        text: 'Interview Appt',
-        day: '26th of Sept 2022',
-        remider: true,
-      }
+      // {
+      //   id: 2,
+      //   text: 'Interview Appt',
+      //   day: '26th of Sept 2022',
+      //   reminder: true,
+      // }
         
       
 
   ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() + 10000 + 1)
+    const newTask = { id, ...task}
+    setTasks([...tasks, newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -31,7 +39,7 @@ const App = () =>{
 
   // Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id)
+    setTasks(tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
 
@@ -39,6 +47,7 @@ const App = () =>{
     <>
     <div className="container">
       <Header/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> 
       ) : ('No Tasks')}
